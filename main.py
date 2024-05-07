80beb0b05182e5aebe619420c769d458afe6da7c
Muyiwa <forevermuyii@gmail.com>
2024-05-07 13:10:02 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import os
+
+import streamlit as st
+from dotenv import load_dotenv
+import google.generativeai as gen_ai
+
+
+# Load environment variables
+load_dotenv()
+
+# Configure Streamlit page settings
+st.set_page_config(
+    page_title="Chat with Gemini-Pro!",
+    page_icon=":brain:",  # Favicon emoji
+    layout="centered",  # Page layout option
+)
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+# Set up Google Gemini-Pro AI model
+gen_ai.configure(api_key=GOOGLE_API_KEY)
+model = gen_ai.GenerativeModel('gemini-pro')
+
+
+# Function to translate roles between Gemini-Pro and Streamlit terminology
+def translate_role_for_streamlit(user_role):
+    if user_role == "model":
+        return "assistant"
+    else:
+        return user_role
+
+
+# Initialize chat session in Streamlit if not already present
+if "chat_session" not in st.session_state:
+    st.session_state.chat_session = model.start_chat(history=[])
+
+
+# Display the chatbot's title on the page
+st.title("🤖 HUNTAH PRO - ChatBot")
+
+# Display the chat history
+for message in st.session_state.chat_session.history:
+    with st.chat_message(translate_role_for_streamlit(message.role)):
+        st.markdown(message.parts[0].text)
+
+# Input field for user's message
+user_prompt = st.chat_input(placeholder="Ask Huntah-Pro...")
+final_prompt = f'''
+you are a bot that is dedicated to statistics and data science.
+if the user prompt is not related to data science and statistics, tell them you can't help them.
+The only situation where you answer a question that is not related to data science and statistics is if
+it is a greeting.
+user prompt: {user_prompt}
+Message if not related to data science and statistics: I'm sorry, I can't help you with that. I am a bot that is dedicated to statistics and data science.
+always answer nicely and professionally. Also, always greet the user politely if and only if they greet you.
+Your answer should not bare a greeting if the user did not greet you.
+
+'''
+if user_prompt:
+    # Add user's message to chat and display it
+    st.chat_message("user").markdown(user_prompt)
+
+    # Send user's message to Gemini-Pro and get the response
+    gemini_response = st.session_state.chat_session.send_message(user_prompt)
+
+    # Display Gemini-Pro's response
+    with st.chat_message("assistant"):
+        st.markdown(gemini_response.text)
